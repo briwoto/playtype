544dfc19712435100ccee9b222c5e3d61e43b07f
Rahul Singh <rahul.beck@gmail.com>
2023-12-05 09:38:14 +0100

    add teardown script to remove cookies

--- src/config/define-config.ts
+++ src/config/define-config.ts
@@ -1,31 +1,18 @@
 import * as path from 'path';
 import { defineConfig } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export const staticConfig = defineConfig({
   testDir: path.resolve(process.cwd(), 'src/specs/'),
   timeout: 20000,
-  /* do not Run tests in files in parallel */
-  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  /* Opt out of parallel tests*/
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://parabank.parasoft.com',
     viewport: null,
     // Run as headless in CI
--- src/config/global-teardown.ts
+++ src/config/global-teardown.ts
@@ -2,8 +2,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 async function globalTeardown() {
-  //const storageFilePath = path.join(process.cwd(), process.env.COOKIE_PATH!);
-  //fs.rmSync(storageFilePath, { recursive: true, force: true });
+  const storageFilePath = path.join(process.cwd(), process.env.COOKIE_PATH!);
+  fs.rmSync(storageFilePath, { recursive: true, force: true });
 }
 
 export default globalTeardown;
